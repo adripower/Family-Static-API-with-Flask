40ea7883994cd1564b524364f11673cca0013003
Adrian Torres <adriantorres.gc@gmail.com>
2024-04-17 23:48:39 +0000

    modified:   src/datastructures.py

--- src/datastructures.py
+++ src/datastructures.py
@@ -11,9 +11,27 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
+        self._next_id = 1
         # example list of members
-        self._members = []
+        self._members = [
+        {"id": self._generateId(),
+            "first_name": "John",
+            "last_name": self.last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]},
+
+        {"id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": self.last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]},
+
+        {"id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": self.last_name,
+            "age": 5,
+            "lucky_numbers": [1]}
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
